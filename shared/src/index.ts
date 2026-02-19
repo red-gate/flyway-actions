92a9b1c9f8d481698d06a1629758b7595c99363b
Buster Major <buster.major@red-gate.com>
2026-02-19 16:22:10 +0000

    export UploadReportOptions type

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,3 +1,4 @@
 export { getFlywayDetails, parseExtraArgs, runFlyway } from "./flyway-runner.js";
 export type { FlywayDetails, FlywayEdition, FlywayRunResult } from "./types.js";
+export type { UploadReportOptions } from "./upload-report.js";
 export { uploadReport } from "./upload-report.js";
