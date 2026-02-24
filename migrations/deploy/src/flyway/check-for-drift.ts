edc9c8b6ec7822f3c9ff259594258d4d4fb512d7
The Milk Man <ajay.ahir@red-gate.com>
2026-02-24 15:19:27 +0000

    extract function for drift detected

--- migrations/deploy/src/flyway/check-for-drift.ts
+++ migrations/deploy/src/flyway/check-for-drift.ts
@@ -16,26 +16,28 @@ const checkForDrift = async (inputs: FlywayMigrationsDeploymentInputs): Promise<
     const driftArgs = getCheckDriftArgs(inputs);
     const result = await runFlyway(driftArgs, inputs.workingDirectory);
 
-    const exitCode = result.exitCode;
-    let driftDetected: boolean | undefined;
-    if (exitCode === 0) {
-      driftDetected = false;
-    } else {
-      const errorOutput = parseErrorOutput(result.stdout);
-      if (errorOutput?.error?.message?.includes("Drift detected")) {
-        driftDetected = true;
-      }
-    }
-
-    core.setOutput("exit-code", exitCode.toString());
-    if (driftDetected !== undefined) {
-      core.setOutput("drift-detected", driftDetected.toString());
-    }
-
+    const driftDetected = isDriftDetected(result.exitCode, result.stdout);
+    setDriftOutput(result.exitCode, driftDetected);
     return !!driftDetected;
   } finally {
     core.endGroup();
   }
+};
+
+const isDriftDetected = (exitCode: number, stdout: string): boolean | undefined => {
+  if (exitCode !== 0) {
+    const errorOutput = parseErrorOutput(stdout);
+    if (errorOutput?.error?.message?.includes("Drift detected")) {
+      return true;
+    }
+    return undefined;
+  }
+  return false;
+};
+
+const setDriftOutput = (exitCode: number, driftDetected: boolean | undefined): void => {
+  core.setOutput("exit-code", exitCode.toString());
+  driftDetected !== undefined && core.setOutput("drift-detected", driftDetected.toString());
 };
 
 export { checkForDrift, getCheckDriftArgs };
